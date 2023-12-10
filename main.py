406b7659361305e2be72bd629d5305e5e196132c
OmerSalomon <102198073+OmerSalomon@users.noreply.github.com>
2023-12-10 16:00:35 +0200

    first commit

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ def main():
             add(num1, num2)
         elif op == 2:
             sub(num1, num2)
+        elif op == 3:
+            mult(num1, num2)
+        elif op == 4:
+            div(num1, num2)
+        elif op == 5:
+            power(num1, num2)
         else:
             print("Invalid choice. Please choose a valid operation.")
 
@@ -23,6 +29,15 @@ def add(num1, num2):
 
 def sub(num1, num2):
     print(num1 * num2)
+
+def mult(num1, num2):
+    print(num1 * num2)
+
+def div(num1, num2):
+    print(num1 / num2)
+
+def power(num1, num2):
+    print(num1 ** num2)
 
 if __name__ == '__main__':
     main()
